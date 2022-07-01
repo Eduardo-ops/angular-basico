a5e85c9e09fea6054d48e905a1b58e3cb2edd09a
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-07-01 20:56:52 -0300

    Técnica template e interpolacao implementado.

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core'
 })
 export class HelloComponent {
 
+    name: String
+    customers: Customer[]
+
+    constructor() {
+        this.name = 'Eduardo Isidoro Gonçalves'
+        let customerI = new Customer('Eduardo', 25)
+        let customerII = new Customer('Norberto Nibeiro', 37)
+        let customerIII = new Customer('Gabriela Evangelista', 26);
+        let customerIV = new Customer('Piuiu abacaxi', 50)
+        //customer.name = 'Eduardo'
+        //customer.age = 25
+        this.customers = [customerI, customerII, customerIII, customerIV]
+    }
+}
+
+class Customer {
+
+    constructor(public name: string, public age: number) {
+
+    }
+
+    //name!: string
+    //age!: number
 }
